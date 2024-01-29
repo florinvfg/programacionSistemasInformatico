77d2a63b0920cf872624886d10fa34dd3086b2cb
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-01-29 20:37:17 +0100

    Add files via upload

--- actividadesResueltas/Unidad3/ActividadResuelta3_5.java
+++ actividadesResueltas/Unidad3/ActividadResuelta3_5.java
@@ -1,6 +1,8 @@
 package Unidad3;
 import java.util.*;
-/*Desarollar un juego que ayude a mejorar el calculo mental de la suma*/
+/*Desarollar un juego que ayude a mejorar el calculo mental de la suma.El jugador tendra que introducir la solucion de la suma de dos numeros
+* aleatorios comprendidos entre 1 y 100.Mientras la solucion sea correcta, el juego continuara.En caso contrario,el
+* programa terminara y mostrara el numero  de operaciones realizadas correctamente*/
 public class ActividadResuelta3_5 {
     public static void main(String[] args) {
        /* System.out.println("Juego de la suma en bucle."
