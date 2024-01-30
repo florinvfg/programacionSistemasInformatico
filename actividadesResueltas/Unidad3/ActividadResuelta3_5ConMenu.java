7d7018f3e391673871a39a42186a994419a60530
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-01-30 19:40:06 +0100

    Add files via upload

--- actividadesResueltas/Unidad3/ActividadResuelta3_5ConMenu.java
+++ actividadesResueltas/Unidad3/ActividadResuelta3_5ConMenu.java
@@ -7,47 +7,50 @@ import javax.swing.*;
 public class ActividadResuelta3_5ConMenu {
     //con monstrar en pantalla
     public static void main(String[] args) {
-String menu=" ";
-byte opcion=0;
-char simbolo=' ';
-String salida=" ";
-int operando1,operando2,resultado=0,resultadoUser=0,contador=0;
-do {
-    menu=" ";
-    menu+="1. Sumar \n";
-    menu+="2.Restar\n";
-    menu+="3.Multiplicar\n";
-    menu+="4.Salir \n";
-    // Se solicita al usuario elegir una opción del menú.
-    opcion=(byte) Integer.parseInt(JOptionPane.showInputDialog(menu));
-    // Se generan dos operandos aleatorios entre 1 y 100.
-    operando1= (int) (Math.random()*100+1);
-    operando2= (int) (Math.random()*100+1);
-    // Se utiliza una estructura switch para realizar la operación correspondiente según la opción elegida.
-    switch (opcion){
-        case 1 ->{
-            simbolo= '+';
-            resultado=operando1 + operando2;
-            salida=operando1 + " " + simbolo + operando2;
+        String menu = " ";
+        byte opcion = 0;
+        char simbolo = ' ';
+        String salida = " ";
+        int operando1, operando2, resultado = 0, resultadoUser = 0, contador = 0;
+        do {
+            menu = " ";
+            menu += "1. Sumar \n";
+            menu += "2.Restar\n";
+            menu += "3.Multiplicar\n";
+            menu += "4.Salir \n";
+            // Se solicita al usuario elegir una opción del menú.
+            opcion = (byte) Integer.parseInt(JOptionPane.showInputDialog(menu));
+            // Se generan dos operandos aleatorios entre 1 y 100.
+            operando1 = (int) (Math.random() * 100 + 1);
+            operando2 = (int) (Math.random() * 100 + 1);
+            // Se utiliza una estructura switch para realizar la operación correspondiente según la opción elegida.
+            switch (opcion) {
+                case 1 -> {
+                    simbolo = '+';
+                    resultado = operando1 + operando2;
+                    salida = operando1 + " " + simbolo + operando2;
+                }
+                case 2 -> {
+                    simbolo = '-';
+                    resultado = operando1 - operando2;
+                    salida = operando1 + " " + simbolo + operando2;
+                }
+                case 3 -> {
+                    simbolo = '*';
+                    resultado = operando1 * operando2;
+                    salida = operando1 + " " + simbolo * operando2;
+                }
+            }
+            if (opcion != 4) {// Si la opción elegida no es salir (4), se solicita al usuario ingresar el resultado.
+                if (opcion != 4) {
+                    resultadoUser = Integer.parseInt(JOptionPane.showInputDialog(salida));
+                }
+                ++contador; // Se incrementa el contador de intentos.
+            }
         }
-        case 2 ->{
-            simbolo= '-';
-            resultado=operando1 - operando2;
-            salida=operando1 + " " + simbolo + operando2;
-        }
-        case 3 ->{
-            simbolo= '*';
-            resultado=operando1 * operando2;
-            salida=operando1 + " " + simbolo * operando2;
-        }
-    }
-    if (opcion!=4) {// Si la opción elegida no es salir (4), se solicita al usuario ingresar el resultado.
-        if (opcion != 4) {
-        resultadoUser=Integer.parseInt(JOptionPane.showInputDialog(salida));
-    }
-    ++contador; // Se incrementa el contador de intentos.
-}while (opcion!=4 && resultadoUser==resultado);
-JOptionPane.showMessageDialog(null,contador);// Se muestra un mensaje con la cantidad de intentos realizados.
+            while (opcion != 4 && resultadoUser == resultado) ;
+            JOptionPane.showMessageDialog(null, contador);// Se muestra un mensaje con la cantidad de intentos realizados.
+
 
     }
 }
