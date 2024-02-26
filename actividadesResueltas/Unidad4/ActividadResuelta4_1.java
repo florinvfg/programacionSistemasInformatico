6d0a51830bf3863448e77d70b9307d5809b4fe75
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-02-26 16:37:14 +0100

    Add files via upload

---
+++ actividadesResueltas/Unidad4/ActividadResuelta4_1.java
@@ -0,0 +1,46 @@
+/*Diseñar la funcion eco() a la que se le pasa como parametro un numero n, y muestra
+* por pantalla n veces el mensaje "Eco..."*/
+//[̲̅Y][̲̅o][̲̅ ][̲̅V][̲̅F][̲̅G]
+import java.util.*;
+
+/*Las soluciones iran acompaniadasde una funcion main que sirva de prueba.El prototipo de la funcion es:
+* void eco(int n).*/
+
+public class ActividadResuelta4_1 {//una clase
+    public static void main(String[] args) {//funcion o metodo
+      /*  Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un numero ➔ ");
+        int n = sc.nextInt();
+        System.out.println("--Antes de llamar a la funcion--");
+        eco (n);//invocamos a la funcion
+        System.out.println("--Despues de llamar a la funcion--");
+    }
+    //La funcion lo unico que hace es mostrar un mensaje repetido mediante un bucle
+    static void eco(int a) {//el parametro no tiene por que llamarse como en la llamada
+        for (int i = 0; i < a; i++) {
+            System.out.println("Eco...");
+        }*/
+        //Segundo ejemplo
+
+
+
+       //se llama las funciones o metodos que van a ejecutar
+        tresSaludos();//llamada de la funcion
+    }
+    public static void tresSaludos(){
+        System.out.println("Te llamo tres veses");
+        for (int i=0;i<3;i++){
+            System.out.println("Hola");
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}
