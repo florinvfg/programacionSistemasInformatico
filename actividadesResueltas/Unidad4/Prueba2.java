f735d4495759c9e35ef011035e6f51d7e96272df
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-02-26 20:00:10 +0100

    Add files via upload

---
+++ actividadesResueltas/Unidad4/Prueba2.java
@@ -0,0 +1,18 @@
+/*El programa solicita al usuario que ingrese su nombre. Luego, muestra el nombre ingresado en la consola con un mensaje
+personalizado indicando "tu nombre es" seguido del nombre proporcionado por el usuario.*/
+//[̲̅Y][̲̅o][̲̅ ][̲̅V][̲̅F][̲̅G]
+import java.util.*;
+public class Prueba2 {
+    public static void main(String[] args) {
+        String nombreUsuario=llamadaNombre();
+        System.out.println("tu nombre es ➥ " + nombreUsuario);
+
+    }
+    public static String llamadaNombre(){
+        String nombre="";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Indica el nombre");
+        nombre=sc.nextLine();
+        return nombre.toUpperCase();
+    }
+}
