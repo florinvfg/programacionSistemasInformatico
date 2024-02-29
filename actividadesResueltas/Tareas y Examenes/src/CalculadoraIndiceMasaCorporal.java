be6371884330cefc72001a54f68c0d1ca108ebc8
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-02-29 19:28:53 +0100

    Add files via upload

--- actividadesResueltas/Tareas y Examenes/src/CalculadoraIndiceMasaCorporal.java
+++ actividadesResueltas/Tareas y Examenes/src/CalculadoraIndiceMasaCorporal.java
@@ -1,6 +1,7 @@
 /*Calcular Indice Masa Corporal,pediendo al usuario que nos indique su altura y su kilos*/
 
 import java.util.*;
+//[̲̅Y][̲̅o][̲̅ ][̲̅V][̲̅F][̲̅G]
 public class CalculadoraIndiceMasaCorporal {
     //IMC=El Índice de Masa Corporal (IMC) es una medida utilizada para estimar si una persona
     // tiene un peso saludable en relación con su altura. Funciona calculando la relación entre el peso y la altura
@@ -44,22 +45,26 @@ public class CalculadoraIndiceMasaCorporal {
         // los estándares de la Organización Mundial de la Salud (OMS).
         if (imc < 18.5) {
             // Si el IMC es menor que 18.5, se considera que la persona tiene bajo peso
-            System.out.println("Interpretación del IMC \uD83D\uDE44➬ Bajo peso");
+            mostrar("Interpretación del IMC \uD83D\uDE44➬ Bajo peso");
         } else if (imc < 25) {
             // Si el IMC no es menor que 18.5 (ya que ya se ha comprobado en la condición anterior)
             // pero es menor que 25, se considera que la persona tiene un peso dentro del rango normal.
-            System.out.println("Interpretación del IMC ➬ Peso normal");
+            mostrar("Interpretación del IMC ➬ Peso normal");
         } else if (imc < 30) {
             // Si el IMC no es menor que 25 (ya que ya se ha comprobado en la condición anterior) pero
             // es menor que 30, se considera que la persona tiene sobrepeso.
-            System.out.println("Interpretación del IMC\uD83E\uDEE8 ➬ Sobrepeso");
+            mostrar("Interpretación del IMC\uD83E\uDEE8 ➬ Sobrepeso");
         } else {
             //Si ninguna de las condiciones anteriores es verdadera (es decir, el IMC es 30 o superior),
             // se considera que la persona tiene obesidad
-            System.out.println("Interpretación del IMC\uD83D\uDE28 ➬ Obesidad");
+            mostrar("\uD83D\uDE28 ➬ Obesidad");
         }
 
     }
+    public static void mostrar(String salida){
+        System.out.println("\t\"Interpretación del IMC"+salida);
+    }
+
 }
 
 
