ea66797c18320c6c7065e221bfc888ab04fd0d01
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-04-02 15:24:55 +0200

    Add files via upload

---
+++ actividadesResueltas/Tema7/src/ActividadResuelta7_9/Clases/Bombilla.java
@@ -0,0 +1,23 @@
+package ActividadResuelta7_9.Clases;
+
+public class Bombilla {
+
+    public static boolean interruptorGeneral = true;
+    private boolean interruptor;
+    public Bombilla() {
+        interruptor = false;
+    }
+    public void enciende(){
+        interruptor = true;
+    }
+    public void apaga(){
+        interruptor = false;
+    }
+    public boolean estado(){
+        return interruptorGeneral && interruptor;
+    }
+    public String muestraEstado() {
+        return estado() ? "Encendida" : "Apagada";
+    }
+
+}
