1005fbec8256ccc6ad1e35b2cf55bb5f537e2be4
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-04-02 21:30:43 +0200

    Add files via upload

--- actividadesResueltas/Tema7/src/ActividadResuelta7_11_Irina/Clases/Lista.java
+++ actividadesResueltas/Tema7/src/ActividadResuelta7_11_Irina/Clases/Lista.java
@@ -8,6 +8,11 @@ public class Lista {
         elemento=new Integer[0];
     }
     //insertar al principio de la lista..crecer en 1 el array
+
+    /**
+     *
+     * @param nuevo
+     */
     public void insertarPrincipio(Integer nuevo){
         //aumentar la tabla
         elemento= Arrays.copyOf(elemento,elemento.length+1);
@@ -16,18 +21,30 @@ public class Lista {
         elemento[0]=nuevo;
     }
     //insertar en el ultimo sitio de la tabla
+
+    /**
+     *
+     * @param nuevo
+     */
     public void insertarUltimo(Integer nuevo){
         elemento=Arrays.copyOf(elemento,elemento.length+1);
         elemento[elemento.length-1]=nuevo;
 
     }
     //obtener el numero de elementos insertados
+
     public void mostrarElementos(){
         System.out.println("Numero de elemntos-> " +elemento.length);
         System.out.println("Todos los elementos-> " +Arrays.deepToString(elemento));
 
     }
     //isertar un elemento pero nos indican el indice donde insertarlo
+
+    /**
+     *
+     * @param indice
+     * @param nuevo
+     */
     public void isertarConIndice(int indice,Integer nuevo){
         elemento=Arrays.copyOf(elemento,elemento.length+1);
         System.arraycopy(elemento,indice,elemento,indice+1,elemento.length-indice-1);
